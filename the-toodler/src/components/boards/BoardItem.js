6d629eba7a7295a402507232d319579964465855
DanielUnnar <danielunnar12@gmail.com>
2023-11-30 14:32:11 +0000

    Delete WIP

--- the-toodler/src/components/boards/BoardItem.js
+++ the-toodler/src/components/boards/BoardItem.js
@@ -1,40 +1,57 @@
-// YourComponent.js
-import React from 'react';
-import { View, Text, ImageBackground, ScrollView, TouchableOpacity, Pressable } from 'react-native';
-import boardsData from '../../Resources/data.json';
-import styles from './BoardItemStyles'
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Button, TouchableOpacity, ImageBackground } from 'react-native';
+import data from '../../resources/data.json';
+import styles from './BoardItemStyles';
+import { useNavigation } from '@react-navigation/native';
 
-const YourComponent = () => {
-  const { boards } = boardsData;
-  const handleBoardPress = (boardId) => {
-    // Add your navigation or other actions here
-  };
-  const handleAddPress = () => {
-    <BoardCreate/>
-  };
+function YourComponent ({navigation}) {
+  const [boards, setBoards] = useState([]);
+  const map1 = data.boards.map(elem => {
+    return {
+      id: elem.id,
+      name: elem.name,
+      thumbnailPhoto: elem.thumbnailPhoto
+    };
+  });
+  function handleDeletePress(item) {
+    map1.map((elem, index, arr) => {
+      if (elem.id === item) {
+        arr.splice(index, 1)
+      }
+    })
+  }
+  useEffect(() => {
+    setBoards(data.boards);
+  }, []);
+
+  const renderBoard = ({ item }) => (
+    <TouchableOpacity key={item.id} onPress={() => console.log(map1)}>
+      <ImageBackground key={item.id} source={{ uri: item.thumbnailPhoto }} style={styles.boardItem}>
+        <View style={styles.textContainer}>
+          <TouchableOpacity style={styles.modify}><Text style={styles.modifytext}>Modify</Text></TouchableOpacity>
+          <Text style={styles.boardTitle}>{item.name}</Text>
+          <TouchableOpacity style={styles.delete} onPress={() => {handleDeletePress(item.id)}} ><Text style={styles.deletetext}>Delete</Text></TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+
+  const addNewBoard = () => {
+    navigation.navigate('Create Board', {boards: data.boards});
+  }
+
+
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>The Toodler</Text>
-      <Pressable style={boards.boardItem} onPress={() => handleAddPress}><Text style={styles.addBoard}>+</Text></Pressable>
-      {boards.map((board) => (
-        <TouchableOpacity
-          key={board.id}
-          onPress={() => handleBoardPress(board.id)}
-          activeOpacity={0.7}
-        >
-          <ImageBackground
-            key={board.id}
-            source={{ uri: board.thumbnailPhoto }}
-            style={styles.boardItem}
-          >
-            <View style={styles.textContainer}>
-              <Text style={styles.boardTitle}>{board.name}</Text>
-            </View>
-          </ImageBackground>
-        </TouchableOpacity>
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      <Button title="Add Board" onPress={addNewBoard} />
+      <FlatList
+        data={boards}
+        renderItem={renderBoard}
+        keyExtractor={item => item.id.toString()}
+      />
+
+    </View>
   );
 };
 
