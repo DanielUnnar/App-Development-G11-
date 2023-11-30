c1ef22908259936afa211e9fd7d6b87e2f406984
Larus Bergmann Hakonarson <larusbergmann@Laruss-MacBook-Pro.local>
2023-11-30 12:19:47 +0000

    modified board item and board create

--- the-toodler/src/components/boards/boardedit/boardcreate/BoardCreate.js
+++ the-toodler/src/components/boards/boardedit/boardcreate/BoardCreate.js
@@ -1,5 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import boardsData from '../../../../Resources/data.json'
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import styles from './BoardCreateStyles'; // Import the styles
 
-const { boards } = boardsData
+const BoardCreate = () => {
+  const [boardName, setBoardName] = useState('');
+  const [thumbnailUrl, setThumbnailUrl] = useState('');
+
+  const handleSavePress = () => {
+    // Save the board
+  }
+
+  return (
+    <View>
+      <Text style={styles.title}>Create a new board</Text>
+      <TextInput
+        placeholder="Board name"
+        style={styles.input}
+        value={boardName}
+        onChangeText={(text) => setBoardName(text)}
+      />
+      <TextInput
+        placeholder="Thumbnail URL"
+        style={styles.input}
+        value={thumbnailUrl}
+        onChangeText={(text) => setThumbnailUrl(text)}
+      />
+      <TouchableOpacity style={styles.saveButton} onPress={handleSavePress}>
+        <Text style={styles.saveButtonText}>Save</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BoardCreate;
---
+++ the-toodler/src/components/boards/boardedit/boarddelete/BoardDelete.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const BoardDelete = () => {
+  return (
+    <View style={styles.container}>
+      <Button></Button> 
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default BoardDelete; 
