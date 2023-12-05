9813b51eb56fe7d3f81478b7799512733c9d7e9a
DanielUnnar <danielunnar12@gmail.com>
2023-12-05 15:00:14 +0000

    Eslint + Phone Call

--- the-contactor/src/views/contact-list-view/ContactListView.js
+++ the-contactor/src/views/contact-list-view/ContactListView.js
@@ -1,24 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, Image, TouchableOpacity, SectionList, TextInput, Button, FlatList } from 'react-native';
+import { Text, View, Image, TouchableOpacity, SectionList, TextInput, Button, Linking } from 'react-native';
 import styles from './ContactListViewStyles';
 import { Icon } from '@rneui/themed';
-import { readAllContacts, addContact, deleteAllJohnFiles } from '../../services/fileService';
+import { readAllContacts, addContact } from '../../services/fileService';
 
 function ContactListView({ navigation }) {
-  const [contacts, setContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredContacts, setFilteredContacts] = useState(null);
+
   const john = {
     name: 'John',
     profileimage: 'https://fortnite.gg/img/items/10730/icon.jpg?3',
-    phoneNumber: '8527722'
-  }
-  const addJohnContact = () => {
-    const john = {
-      name: 'John',
-      profileimage: 'https://fortnite.gg/img/items/10730/icon.jpg?3',
-      phoneNumber: '8527722'
-    };
+    phoneNumber: '8526969',
+  };
 
-    addContact(john, fetchContacts);
+  const addJohnContact = async () => {
+    await addContact(john);
+    fetchContacts();
   };
 
   const fetchContacts = async () => {
@@ -28,21 +27,12 @@ function ContactListView({ navigation }) {
     }
   };
 
-  
   useEffect(() => {
-    async function fetchContacts() {
-      const allContacts = await readAllContacts();
-      if (allContacts !== null) {
-        setContacts(allContacts)
-      }
-    }
-
     fetchContacts();
   }, []);
 
-
   const renderContacts = ({ item }) => (
-    <TouchableOpacity key={item.uuid} onPress={() => { ContactDetail(item) }}>
+    <TouchableOpacity key={item.uuid} onPress={() => ContactDetail(item)}>
       <View style={styles.view}>
         <Image style={styles.image} source={{ uri: item.profileimage }} />
         <Text style={styles.text}>{item.name}</Text>
@@ -53,25 +43,68 @@ function ContactListView({ navigation }) {
 
   const ContactDetail = (item) => {
     navigation.navigate('Contact Details', { item: item });
-  }
+  };
+
+  const sortedContacts = contacts.sort((a, b) => a.name.localeCompare(b.name));
+
+  const groupedContacts = sortedContacts.reduce((acc, contact) => {
+    const firstLetter = contact.name[0].toUpperCase();
+    if (!acc[firstLetter]) {
+      acc[firstLetter] = [];
+    }
+    acc[firstLetter].push(contact);
+    return acc;
+  }, {});
+
+  const sections = Object.entries(groupedContacts).map(([letter, data]) => ({
+    title: letter,
+    data,
+  }));
+
+  const handleSearch = (query) => {
+    const lowerCaseQuery = query.toLowerCase();
+
+    if (lowerCaseQuery.trim() === '') {
+      setFilteredContacts(null);
+    } else {
+      const filteredContacts = sortedContacts.filter((contact) => {
+        const filteredName = contact.name.toLowerCase().includes(lowerCaseQuery);
+        return filteredName;
+      });
+
+      setFilteredContacts([{ title: 'Search Results', data: filteredContacts }]);
+    }
+
+    setSearchQuery(query);
+  };
+
+  const renderSectionHeader = ({ section: { title } }) => (
+    <View style={styles.sectionHeader}>
+      <Text style={styles.sectionHeaderText}>{title}</Text>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
+      <Button title='Create John' onPress={() => {Linking.openURL(`tel:8526969`)}} />
       <View style={styles.header}>
-        <Button title='Create John' onPress={() => addJohnContact()}/>
+        <TextInput
+          placeholder='Search'
+          style={styles.searchbar}
+          value={searchQuery}
+          onChangeText={handleSearch}
+        />
         <TouchableOpacity onPress={() => navigation.navigate('Create Contact')}>
           <Text style={styles.newContactBtn}>+</Text>
         </TouchableOpacity>
-
       </View>
-      {contacts.length > 0 ? (
-      <FlatList 
-      data={contacts}
-      renderItem={renderContacts}
-      keyExtractor={item => item.uuid.toString()}/>
-      ) : (
-        <Text>No Contacts Found</Text>
-      )}
+
+      <SectionList
+        sections={filteredContacts || sections}
+        renderItem={renderContacts}
+        renderSectionHeader={renderSectionHeader}
+        keyExtractor={(item, index) => (item && item.uuid ? item.uuid.toString() : index.toString())}
+      />
     </View>
   );
 }
