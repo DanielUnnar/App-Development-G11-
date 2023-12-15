e3c0fd9f6f596aca9d5c5e7e0871d35fa2182ee2
bjarkib22 <bjarkib22@ru.is>
2023-12-15 12:53:49 +0000

    fix

--- dr-cinema/src/views/home/home.jsx
+++ dr-cinema/src/views/home/home.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, Dimensions, ImageBackground } from 'react-native';
 import { getCinemas } from '../../services/APIservice'
 
+const windowWidth = Dimensions.get('window').width;
+
 export function HomeScreen({navigation, route}) {
   const [data, setData] = useState('')
   async function handleCinemas() {
@@ -10,8 +12,6 @@ export function HomeScreen({navigation, route}) {
       const cinemas = await getCinemas();
       const sortedCinemas = cinemas.slice().sort((a, b) => a.name.localeCompare(b.name));
       setData(sortedCinemas)
-
-const windowWidth = Dimensions.get('window').width;
 
     } catch (error) {
       console.error('Error:', error);
